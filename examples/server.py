165144fb6c2b0d576f2e6b08b9b9088745e815bd
ReddyyZ <41487053+ReddyyZ@users.noreply.github.com>
2020-05-27 13:06:13 -0300

    1.0 Version

---
+++ examples/server.py
@@ -0,0 +1,27 @@
+import rssh.server as rssh
+from threading import Thread
+from time import sleep
+
+ssh = None
+
+def start():
+    global ssh
+
+    ssh = rssh.SSHServer('127.0.0.1')
+    print('Key generated')
+    ssh.start()
+    print('Server started')
+
+Thread(target=start).start()
+
+while True:
+    print("RSSH - Github: https://github.com/ReddyyZ/rssh\n1.List online clients\n2.Kill")
+    option = int(input('-> '))
+    if option == 1:
+        for i in ssh.clients:
+            print(ssh.clients[i])
+    elif option == 2:
+        ip = str(input('-> '))
+        ssh.clients[ip].kill()
+    else:
+        print('Invalid option')
